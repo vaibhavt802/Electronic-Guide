6269cf249ea6a2c45a7aed1456f3c340b4a88111
vaibhav thakur <vaibhavt802@gmail.com>
2020-02-19 11:11:35 +0530

    Working on four band resistor activity

--- app/src/main/java/com/example/electronicguide/MainActivity.java
+++ app/src/main/java/com/example/electronicguide/MainActivity.java
@@ -28,8 +28,8 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this,ResistorActivity.class);
                 startActivity(intent);
-
             }
         });
+
     }
 }
--- app/src/main/java/com/example/electronicguide/ResistorActivity.java
+++ app/src/main/java/com/example/electronicguide/ResistorActivity.java
@@ -1,15 +1,24 @@
 package com.example.electronicguide;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.view.View;
 
 public class ResistorActivity extends AppCompatActivity {
-
+    private CardView mfourband;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resistor);
-
-
+        mfourband = (CardView)findViewById(R.id.fourband);
+        mfourband.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ResistorActivity.this, FourBResistorActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
